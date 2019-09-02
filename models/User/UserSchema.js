13decb81db18ce6186156d9f48f7e81c08b09f6f
Bryce Davidson <lifebryce@gmail.com>
2019-09-01 22:04:45 -0700

    cls

--- models/User/UserSchema.js
+++ models/User/UserSchema.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const bcrypt   = require('bcrypt-nodejs');
-const Locations = require('../Location/LocationSchema').Schema;
 
 var userSchema = mongoose.Schema({
     local: {
@@ -25,7 +24,7 @@ var userSchema = mongoose.Schema({
     },
     photos: {
       profile: String,
-      profile500: String,
+      profilepx500: String,
       banner: String
     }
 });
