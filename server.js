6d4170ce2f863b2319e02af464672adfc3ff6d3d
Bryce Davidson <lifebryce@gmail.com>
2019-09-07 23:43:42 -0700

    Restructure async/await

--- server.js
+++ server.js
@@ -4,23 +4,6 @@ const app = require("./app.js");
 
 PORT = process.env.PORT || 4000;
 
-// ERROR HANDELING ------------------------------------------
-
-app.use(function mongoErrors(err, req, res, next) {
-  if (res.headersSent) return next(err);
-  if (err.name === 'CastError') {
-    return res.status(500).send({name: err.name, message: err.message})
-  }
-  if (err.name === 'MongoError') {
-    return res.status(500).send({name: err.name, message: err.errmsg})
-  }
-  if(err.name == 'ValidationError') {
-    console.log(err)
-    return res.status(500).send({name: err.name, message: err.message})
-  }
-  else next();
-});
-
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
 });
