2e1c16549a17701f944ee3effed4bf8d3a52d5f8
Bryce <bryce_davidson@icloud.com>
2019-09-08 18:21:15 -0700

    Format on controllers

--- controllers/Models/Days/Day-Controller.js
+++ controllers/Models/Days/Day-Controller.js
@@ -1,21 +1,21 @@
-const User              = require('../../../models/User/UserSchema');
-const Day               = require('../../../models/Day/DaySchema');
-const { isOwner }       = require('../../../util/local-functions/instance-validation');
+const User = require('../../../models/User/UserSchema');
+const Day = require('../../../models/Day/DaySchema');
+const { isOwner } = require('../../../util/local-functions/instance-validation');
 // const { DAYBUCKET }     = require('../../../config/keys').AWS;
-const upload            = require('../../../util/middleware/photo-upload-util');
-const flatten           = require('flat');
-const quarantineUpdate  = require('../../../util/local-functions/quarantine-update');
+const upload = require('../../../util/middleware/photo-upload-util');
+const flatten = require('flat');
+const quarantineUpdate = require('../../../util/local-functions/quarantine-update');
 
 const AWS = require('aws-sdk')
 const S3 = new AWS.S3()
 
-const getDays = async function(req, res, next) {
-    let { text, tags, min_budget, max_budget, paths, omit, pagenation} = req.query;
+const getDays = async function (req, res, next) {
+    let { text, tags, min_budget, max_budget, paths, omit, pagenation } = req.query;
     let query = {};
     if (paths) { paths = paths.replace(/,/g, ' ') };
-    if (omit)  { omit = omit.split(',').map(item => `-${item}`).join(' ') };
-    if (tags)  { query['tags'] = { $all: tags.split(',') } }; 
-    if (text)  { query.$text = { $search: text } };
+    if (omit) { omit = omit.split(',').map(item => `-${item}`).join(' ') };
+    if (tags) { query['tags'] = { $all: tags.split(',') } };
+    if (text) { query.$text = { $search: text } };
     if (min_budget || max_budget) {
         const mb = query['budget.middleBound'] = {};
         if (min_budget) mb.$gte = min_budget;
@@ -23,18 +23,18 @@ const getDays = async function(req, res, next) {
     };
 
     Day.find(query)
-    .where({'settings.public': true})
-    .select(paths)
-    .select(omit)
-    .limit(Number(pagenation))
-    .then(docs => {
-        delete query;
-        return res.send(docs);
-    }).catch(next);
+        .where({ 'settings.public': true })
+        .select(paths)
+        .select(omit)
+        .limit(Number(pagenation))
+        .then(docs => {
+            delete query;
+            return res.send(docs);
+        }).catch(next);
 }
 
-const postDay = async function(req, res, next) {
-    let {name, description, tags, upperBound, lowerBound, public} = req.body;
+const postDay = async function (req, res, next) {
+    let { name, description, tags, upperBound, lowerBound, public } = req.body;
     let slug = slugify(name);
     let uniqueid = await recursiveGenerateUniqueUrlid(slug, Day);
     return new Day({
@@ -43,15 +43,15 @@ const postDay = async function(req, res, next) {
         name,
         description,
         tags,
-        meta: {upperBound, lowerBound, urlid: uniqueid},
-        settings: {public}
+        meta: { upperBound, lowerBound, urlid: uniqueid },
+        settings: { public }
     })
-    .save()
-    .then(ntrip => res.status(201).send(ntrip))
-    .catch(next);
+        .save()
+        .then(ntrip => res.status(201).send(ntrip))
+        .catch(next);
 }
 
-const getDay = async function(req, res, next) {
+const getDay = async function (req, res, next) {
     let dayid = req.params.id;
     Day.findById(dayid)
         .then(day => {
@@ -60,95 +60,95 @@ const getDay = async function(req, res, next) {
                 return res.send(day);
             if (!day.settings.public)
                 return unauthorizedMsg(res);
-            else return Day.findByIdAndUpdate(dayid, { $inc: { 'meta.viewCount': 1 }})
-                                .then(uday => {return res.send(uday)});
+            else return Day.findByIdAndUpdate(dayid, { $inc: { 'meta.viewCount': 1 } })
+                .then(uday => { return res.send(uday) });
         })
         .catch(next);
 }
 
-const updateDay = async function(req, res, next) {
+const updateDay = async function (req, res, next) {
     let dayid = req.params.id;
     let update = flatten(req.body);
     if (update.name)
         update.slug = slugify(update.name);
-    
+
     try {
-    update = await quarantineUpdate(update);
-    let dayTobeModified = await Day.findById(dayid);
-    if (!dayTobeModified) return notExistMsg('Day', res);
-    if (isOwner(dayTobeModified, req.user)) {
-        let updatedday = await Day.findByIdAndUpdate(dayid, update, {new: true});
-        return res.send(updatedday);
-    } else 
-        return unauthorizedMsg(res);
+        update = await quarantineUpdate(update);
+        let dayTobeModified = await Day.findById(dayid);
+        if (!dayTobeModified) return notExistMsg('Day', res);
+        if (isOwner(dayTobeModified, req.user)) {
+            let updatedday = await Day.findByIdAndUpdate(dayid, update, { new: true });
+            return res.send(updatedday);
+        } else
+            return unauthorizedMsg(res);
     } catch (err) {
-        if (err.code === 'Immutable') 
+        if (err.code === 'Immutable')
             return res.status(err.code).json(err.msg)
         else
-            next(err) 
+            next(err)
     };
 }
 
-const deleteDay = async function(req, res, next) {
+const deleteDay = async function (req, res, next) {
     let dayid = req.params.id;
     try {
-    let day = await Day.findById(dayid);
-    if (!day) return notExistMsg('Day', res);
-    if (isOwner(day, req.user)) {
-        await day.remove();
-        return res.status(200);
-    } else 
-        return unauthorizedMsg(res);
-    } catch (err) {next(err)};
+        let day = await Day.findById(dayid);
+        if (!day) return notExistMsg('Day', res);
+        if (isOwner(day, req.user)) {
+            await day.remove();
+            return res.status(200);
+        } else
+            return unauthorizedMsg(res);
+    } catch (err) { next(err) };
 }
 
-const getDayLocations = async function(req, res, next) {
+const getDayLocations = async function (req, res, next) {
     let dayid = req.params.id;
     try {
-    let day = await Day.findById(dayid).populate('locations');
-    if (!day) return notExistMsg('Day', res);
-    if (!day.locations) return res.status(404).json({msg: "day currently has 0 locations"});
-    else 
-        return res.send(day.locations);
-    } catch (err) {next(err)};
+        let day = await Day.findById(dayid).populate('locations');
+        if (!day) return notExistMsg('Day', res);
+        if (!day.locations) return res.status(404).json({ msg: "day currently has 0 locations" });
+        else
+            return res.send(day.locations);
+    } catch (err) { next(err) };
 }
 
-const addLocationToDay = async function(req, res, next) {
+const addLocationToDay = async function (req, res, next) {
     let dayid = req.params.id;
-    let locationid  = req.query.locationid;
-    if(!ObjectId.isValid(locationid))
-        return res.status(422).json({msg: `Invalid location id: ${locationid}`});
-    try {    
-    let locationToBeAdded = await Location.findById(locationid);
-    if (!locationToBeAdded) return notExistMsg('Location', res);
-    let dayToAddlocationTo = await day.findById(dayid);
-    if (!dayToAddlocationTo) return notExistMsg('Day', res);
-    if (isOwner(dayToAddlocationTo, req.user)) {
-        let dayWithLocationAdded = await day.findByIdAndUpdate(dayid, { $push: { locations: locationid }}, {new: true});
-        return res.send(dayWithLocationAdded);
-    } else 
-        return unauthorizedMsg(res);
-    } catch (err) {next(err)};
+    let locationid = req.query.locationid;
+    if (!ObjectId.isValid(locationid))
+        return res.status(422).json({ msg: `Invalid location id: ${locationid}` });
+    try {
+        let locationToBeAdded = await Location.findById(locationid);
+        if (!locationToBeAdded) return notExistMsg('Location', res);
+        let dayToAddlocationTo = await day.findById(dayid);
+        if (!dayToAddlocationTo) return notExistMsg('Day', res);
+        if (isOwner(dayToAddlocationTo, req.user)) {
+            let dayWithLocationAdded = await day.findByIdAndUpdate(dayid, { $push: { locations: locationid } }, { new: true });
+            return res.send(dayWithLocationAdded);
+        } else
+            return unauthorizedMsg(res);
+    } catch (err) { next(err) };
 }
 
-const deleteLocationsFromDay = async function(req, res, next) {
+const deleteLocationsFromDay = async function (req, res, next) {
     let dayid = req.params.id;
     let locationids = req.query.locationids.split(',');
-    if (!locationids) return res.status(400).json({msg: 'Please Provide at least one locations.'});
+    if (!locationids) return res.status(400).json({ msg: 'Please Provide at least one locations.' });
     locationids.forEach(id => {
         if (!ObjectId.isValid(id))
-            return res.status(422).json({msg: "Invalid location id.", id});
+            return res.status(422).json({ msg: "Invalid location id.", id });
     });
 
     try {
-    let dayTobeModified = await Day.findById(dayid);
-    if (!dayTobeModified) return notExistMsg('Day', res);
-    if (isOwner(dayTobeModified, req.user)) {
-        let dayWithlocationRemoved = await Day.findByIdAndUpdate(dayid, { $pullAll: { locations: locationids } }, {new: true});
-        return res.send(dayWithlocationRemoved);
-    } else 
-        return unauthorizedMsg(res);
-    } catch (err) {next(err)};
+        let dayTobeModified = await Day.findById(dayid);
+        if (!dayTobeModified) return notExistMsg('Day', res);
+        if (isOwner(dayTobeModified, req.user)) {
+            let dayWithlocationRemoved = await Day.findByIdAndUpdate(dayid, { $pullAll: { locations: locationids } }, { new: true });
+            return res.send(dayWithlocationRemoved);
+        } else
+            return unauthorizedMsg(res);
+    } catch (err) { next(err) };
 }
 
 module.exports = {
--- controllers/Models/Trip/Trip-Controller.js
+++ controllers/Models/Trip/Trip-Controller.js
@@ -1,41 +1,41 @@
-const Trip                          = require('../../../models/Trip/TripSchema');
-const Day                           = require('../../../models/Day/DaySchema');
-const User                          = require('../../../models/User/UserSchema');
-const { 
+const Trip = require('../../../models/Trip/TripSchema');
+const Day = require('../../../models/Day/DaySchema');
+const User = require('../../../models/User/UserSchema');
+const {
     isOwner,
     keysContainString
-}                                   = require('../../../util/local-functions/instance-validation');
+} = require('../../../util/local-functions/instance-validation');
 // const { TRIPBUCKET }                = require('../../../config/keys').AWS;
-const upload                        = require('../../../util/middleware/photo-upload-util');
-const flatten                       = require('flat');
+const upload = require('../../../util/middleware/photo-upload-util');
+const flatten = require('flat');
 
-const ObjectId                      = require('mongoose').Types.ObjectId;
-const recursiveGenerateUniqueUrlid  = require('../../../util/local-functions/generate-unique-urlid');
-const slugify                       = require('../../../util/local-functions/slugify-string');
+const ObjectId = require('mongoose').Types.ObjectId;
+const recursiveGenerateUniqueUrlid = require('../../../util/local-functions/generate-unique-urlid');
+const slugify = require('../../../util/local-functions/slugify-string');
 
-const notExistMsg                   = require('../../../util/errors/resource-does-not-exist-msg');
-const unauthorizedMsg               = require('../../../util/errors/unauthorized-msg');
+const notExistMsg = require('../../../util/errors/resource-does-not-exist-msg');
+const unauthorizedMsg = require('../../../util/errors/unauthorized-msg');
 
 const AWS = require('aws-sdk')
 const S3 = new AWS.S3()
 
 // /trips ----------------------------------------------------------------
 
-const getTrips = async function(req, res, next) {
-    let { text, tags, min_budget, max_budget, paths, omit, pagenation} = req.query;
-        let query = {};
-        if (paths) { paths = paths.replace(/,/g, ' ') };
-        if (omit)  { omit = omit.split(',').map(item => `-${item}`).join(' ') };
-        if (tags)  { query['tags'] = { $all: tags.split(',') } }; 
-        if (text)  { query.$text = { $search: text } };
-        if (min_budget || max_budget) {
-            const mb = query['budget.middleBound'] = {};
-            if (min_budget) mb.$gte = min_budget;
-            if (max_budget) mb.$lte = max_budget;
-        };
+const getTrips = async function (req, res, next) {
+    let { text, tags, min_budget, max_budget, paths, omit, pagenation } = req.query;
+    let query = {};
+    if (paths) { paths = paths.replace(/,/g, ' ') };
+    if (omit) { omit = omit.split(',').map(item => `-${item}`).join(' ') };
+    if (tags) { query['tags'] = { $all: tags.split(',') } };
+    if (text) { query.$text = { $search: text } };
+    if (min_budget || max_budget) {
+        const mb = query['budget.middleBound'] = {};
+        if (min_budget) mb.$gte = min_budget;
+        if (max_budget) mb.$lte = max_budget;
+    };
 
-        Trip.find(query)
-        .where({'settings.public': true})
+    Trip.find(query)
+        .where({ 'settings.public': true })
         .select(paths)
         .select(omit)
         .limit(Number(pagenation))
@@ -45,8 +45,8 @@ const getTrips = async function(req, res, next) {
         }).catch(next);
 }
 
-const postTrip = async function(req, res, next) {
-    let {name, description, tags, upperBound, lowerBound, public} = req.body;
+const postTrip = async function (req, res, next) {
+    let { name, description, tags, upperBound, lowerBound, public } = req.body;
     let slug = slugify(name);
     let uniqueid = await recursiveGenerateUniqueUrlid(slug, Trip);
     return new Trip({
@@ -55,17 +55,17 @@ const postTrip = async function(req, res, next) {
         name,
         description,
         tags,
-        meta: {upperBound, lowerBound, urlid: uniqueid},
-        settings: {public}
+        meta: { upperBound, lowerBound, urlid: uniqueid },
+        settings: { public }
     })
-    .save()
-    .then(ntrip => res.status(201).send(ntrip))
-    .catch(next);
+        .save()
+        .then(ntrip => res.status(201).send(ntrip))
+        .catch(next);
 }
 
 // //trips/:id ?populate
 
-const getTrip = async function(req, res, next) {
+const getTrip = async function (req, res, next) {
     let tripid = req.params.id;
     Trip.findById(tripid)
         .then(trip => {
@@ -74,113 +74,113 @@ const getTrip = async function(req, res, next) {
                 return res.send(trip);
             if (!trip.settings.public)
                 return unauthorizedMsg(res);
-            else return Trip.findByIdAndUpdate(tripid, { $inc: { 'meta.viewCount': 1 }})
-                                .then(utrip => {return res.send(utrip)});
+            else return Trip.findByIdAndUpdate(tripid, { $inc: { 'meta.viewCount': 1 } })
+                .then(utrip => { return res.send(utrip) });
         })
         .catch(next);
 }
 
-const updateTrip = async function(req, res, next) {
+const updateTrip = async function (req, res, next) {
     let tripid = req.params.id;
     let update = flatten(req.body);
     if (update.name)
         update.slug = slugify(update.name);
 
     if (keysContainString('meta', update))
-        return res.status(403).json({msg: 'Unable to update on immutable path "meta".'});
+        return res.status(403).json({ msg: 'Unable to update on immutable path "meta".' });
     if (keysContainString('slug', update))
-        return res.status(403).json({msg: 'Unable to update on immutable path "slug".'});
+        return res.status(403).json({ msg: 'Unable to update on immutable path "slug".' });
     try {
-    let tripTobeModified = await Trip.findById(tripid);
-    if (!tripTobeModified) return notExistMsg('Trip', res);
-    if (isOwner(tripTobeModified, req.user)) {
-        let updatedTrip = await Trip.findByIdAndUpdate(tripid, update, {new: true});
-        return res.send(updatedTrip);
-    } else 
-        return unauthorizedMsg(res);
+        let tripTobeModified = await Trip.findById(tripid);
+        if (!tripTobeModified) return notExistMsg('Trip', res);
+        if (isOwner(tripTobeModified, req.user)) {
+            let updatedTrip = await Trip.findByIdAndUpdate(tripid, update, { new: true });
+            return res.send(updatedTrip);
+        } else
+            return unauthorizedMsg(res);
     } catch (err) { next(err) };
 }
 
 // add error message util
 
-const deleteTrip = async function(req, res, next) {
+const deleteTrip = async function (req, res, next) {
     let tripid = req.params.id;
     try {
-    let trip = await Trip.findById(tripid);
-    if (!trip) return notExistMsg('Trip', res);
-    if (isOwner(trip, req.user)) {
-        await trip.remove();
-        return res.status(200);
-    } else 
-        return unauthorizedMsg(res);
-    } catch (err) {next(err)};
+        let trip = await Trip.findById(tripid);
+        if (!trip) return notExistMsg('Trip', res);
+        if (isOwner(trip, req.user)) {
+            await trip.remove();
+            return res.status(200);
+        } else
+            return unauthorizedMsg(res);
+    } catch (err) { next(err) };
 }
 
-const getTripDays = async function(req, res, next) {
+const getTripDays = async function (req, res, next) {
     let tripid = req.params.id;
     try {
-    let trip = await Trip.findById(tripid).populate('days');
-    if (!trip) return notExistMsg('Trip', res);
-    if (!trip.days) return res.status(404).json({msg: "Trip currently has 0 days"});
-    else 
-        return res.send(trip.days);
-    } catch (err) {next(err)};
+        let trip = await Trip.findById(tripid).populate('days');
+        if (!trip) return notExistMsg('Trip', res);
+        if (!trip.days) return res.status(404).json({ msg: "Trip currently has 0 days" });
+        else
+            return res.send(trip.days);
+    } catch (err) { next(err) };
 }
 
-const addDayToTrip = async function(req, res, next) {
+const addDayToTrip = async function (req, res, next) {
     let tripid = req.params.id;
-    let dayid  = req.query.dayid;
-    if(!ObjectId.isValid(dayid))
-        return res.status(422).json({msg: `Invalid day id: ${dayid}`});
-    try {    
-    let dayToBeAdded = await Day.findById(dayid);
-    if (!dayToBeAdded) return notExistMsg('Day', res);
-    let tripToAddDayTo = await Trip.findById(tripid);
-    if (!tripToAddDayTo) return notExistMsg('Trip', res);
-    if (isOwner(tripToAddDayTo, req.user)) {
-        let utrip = await Trip.findByIdAndUpdate(tripid, { $push: { days: dayid }}, {new: true});
-        return res.send(utrip);
-    } else 
-        return unauthorizedMsg(res);
-    } catch (err) {next(err)};
+    let dayid = req.query.dayid;
+    if (!ObjectId.isValid(dayid))
+        return res.status(422).json({ msg: `Invalid day id: ${dayid}` });
+    try {
+        let dayToBeAdded = await Day.findById(dayid);
+        if (!dayToBeAdded) return notExistMsg('Day', res);
+        let tripToAddDayTo = await Trip.findById(tripid);
+        if (!tripToAddDayTo) return notExistMsg('Trip', res);
+        if (isOwner(tripToAddDayTo, req.user)) {
+            let utrip = await Trip.findByIdAndUpdate(tripid, { $push: { days: dayid } }, { new: true });
+            return res.send(utrip);
+        } else
+            return unauthorizedMsg(res);
+    } catch (err) { next(err) };
 }
 
-const deleteDaysFromTrip = async function(req, res, next) {
+const deleteDaysFromTrip = async function (req, res, next) {
     let tripid = req.params.id;
     let dayids = req.query.dayids.split(',');
-    if (!dayids) return res.status(400).json({msg: 'Please Provide at least one days.'});
+    if (!dayids) return res.status(400).json({ msg: 'Please Provide at least one days.' });
     dayids.forEach(id => {
         if (!ObjectId.isValid(id))
-            return res.status(422).json({msg: "Invalid day id.", id});
+            return res.status(422).json({ msg: "Invalid day id.", id });
     });
 
     try {
-    let tripTobeModified = await Trip.findById(tripid);
-    if (!tripTobeModified) return notExistMsg('Trip', res);
-    if (isOwner(tripTobeModified, req.user)) {
-        let tripWithDayRemoved = await Trip.findByIdAndUpdate(tripid, { $pullAll: { days: dayids } }, {new: true});
-        return res.send(tripWithDayRemoved);
-    } else 
-        return unauthorizedMsg(res);
-    } catch (err) {next(err)};
+        let tripTobeModified = await Trip.findById(tripid);
+        if (!tripTobeModified) return notExistMsg('Trip', res);
+        if (isOwner(tripTobeModified, req.user)) {
+            let tripWithDayRemoved = await Trip.findByIdAndUpdate(tripid, { $pullAll: { days: dayids } }, { new: true });
+            return res.send(tripWithDayRemoved);
+        } else
+            return unauthorizedMsg(res);
+    } catch (err) { next(err) };
 }
 
-const changeDaysPublicStatus = async function(req, res, next) {
-
-}
-
-const likeTrip = async function(req, res, next) {
+const changeDaysPublicStatus = async function (req, res, next) {
 
 }
 
-const commentTrip = async function(req, res, next) {
+const likeTrip = async function (req, res, next) {
+
+}
+
+const commentTrip = async function (req, res, next) {
 
 }
 
 module.exports = {
     tripsRoot: {
         getTrips,
-        postTrip 
+        postTrip
     },
     tripResource: {
         getTrip,
@@ -191,8 +191,8 @@ module.exports = {
         deleteDaysFromTrip,
     },
     tripMeta: {
-            likeTrip,
-            commentTrip,
-            changeDaysPublicStatus
+        likeTrip,
+        commentTrip,
+        changeDaysPublicStatus
     }
 };
