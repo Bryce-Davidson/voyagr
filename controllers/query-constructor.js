51429469ad8bcab6c6c9608e0ebecea94650f463
Bryce <bryce_davidson@icloud.com>
2019-09-16 23:45:08 -0700

    Working tripquery builder

--- controllers/query-constructor.js
+++ controllers/query-constructor.js
@@ -1,10 +1,12 @@
 
 class TripsQuery {
-    constructor() {
+    constructor(featured) {
+        this.pipeline = [];
         this.$match = {};
         this.$project = {};
-        this.$limit = {};
         this.$addFields = {};
+        this.$limit = {};
+        this.featured = featured;
     }
 
     text(text) {
@@ -25,27 +27,64 @@ class TripsQuery {
         return this;
     }
 
-    feature(type) {
-        
+    _set_featured(type) {
+        let stages = [{
+            $addFields: {
+                featuredScore: {
+                    $add: [
+                        { $multiply: ["$meta.likes", 2] },
+                        { $multiply: ["$meta.numberOfShares", 3] },
+                        '$meta.viewCount'
+                    ]
+                }
+            }
+        },
+        { "$sort": { 'featuredScore': -1 } },
+        { $project: { 'featuredScore': 0 } }
+        ];
+        for (let i in stages) {
+            this.pipeline.push(stages[i])
+        }
+    }
+
+    select(paths, omit) {
+        paths.replace(/\s+/g, '').split(',').forEach(p => {
+            this.$project[p] = 1;
+        });
+        omit.replace(/\s+/g, '').split(',').forEach(p => {
+            this.$project[p] = 0;
+        });
+        return this;
+    }
+
+    limit(pagenation) {
+
     }
 
     build() {
-        let pipeline = [];
         for (let stage in this) {
-            if ((Object.entries(this[stage]).length !== 0)) {
-                pipeline.push({ [stage]: this[stage] })
+            if ((this[stage] !== this.pipeline) && (Object.entries(this[stage]).length !== 0)) {
+                this.pipeline.push({ [stage]: this[stage] })
             } else
                 continue
         }
-        return pipeline;
+        if (this.featured) this._set_featured()
+        return this.pipeline;
     }
 
     then() { return this }
 }
 
 
-let new_pipe = new TripsQuery().text("Hello There").budget(100, 50).tags('some,tags')
-console.log(new_pipe.$match)
+let new_pipe = new TripsQuery(true)
+    .text("Hello There")
+    .budget(100, 50)
+    .tags('some,tags')
+    .select('name,description', 'crazy')
+    .build()
+console.log(new_pipe)
+
+
 // class Mongo_Pipeline {
 //     constructor({text, tags, paths, omit, featured, min_budget, max_budget, pagenation}) {
 //         this.pipeline = [];
